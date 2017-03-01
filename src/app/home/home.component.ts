153c30624a0abaa73e157c043ba676aefe601934
tclass <tobias.klasse@googlemail.com>
2017-03-01 23:00:50 +0100

    I “promise” you, this shit doesn’t work  💩

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { MdDialog } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
 import { CreateDialogComponent } from './create-dialog/create-dialog.component';
 import { TVComponent } from './tv/tv.component';
-import { ListService } from './list.service';
+import { ListService, List } from './list.service';
 
 @Component({
   selector: 'app-home',
@@ -16,18 +16,22 @@ export class HomeComponent implements OnInit {
 
   private tvurl: string;
   private sanitizer;
-  private lists;
+  private lists = [];
 
   constructor(private _sanitizer: DomSanitizer, public dialog: MdDialog, private listService: ListService) {
     this.sanitizer = this._sanitizer;
-    this.lists = listService.getLists().then(this.lists, list => list);
 
+    listService.getLists().then(l => {
+      this.lists = l;
+    });
   }
 
   ngOnInit() {
     for (const list of this.lists) {
       for (const video of list.videos) {
+          console.log(video.frame)
         if (video != null) {
+          console.log(video.frame)
           video.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(video.frame);
         }
       }
